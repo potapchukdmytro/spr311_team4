f1a2ae048d9205f377617f6a14591e69a6f8c0db
annavas03 <Admin@DESKTOP-E4CE51S>
2025-05-22 21:05:53 +0500

    Додала AutoMapper і також зробила ImageService а також зробила контроллердо нього ImageController. Перевірила ніби прауює все, повиправляла деякі моменти в коді

---
+++ team4.BLL/Mapping/CategoryProfile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using team4.BLL.Dtos.Category;
+using team4.DAL.Entities;
+using AutoMapper;
+
+namespace team4.BLL.Mapping
+{
+    public class CategoryProfile : Profile
+    {
+        public CategoryProfile()
+        {
+            CreateMap<CategoryEntity, CategoryDto>().ReverseMap();
+
+            CreateMap<CreateCategoryDto, CategoryEntity>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore());
+
+            CreateMap<UpdateCategoryDto, CategoryEntity>();
+        }
+    }
+}
